8293d29be51160e93a64adbef396d2ac0da8b615
Jonathas Tavares <jonathastavares@id.uff.br>
2020-11-17 19:49:14 -0300

    Created method to format the html file to have styles

--- bin/main.rb
+++ bin/main.rb
@@ -3,6 +3,7 @@ require_relative '../lib/search.rb'
 require_relative '../lib/display.rb'
 
 def check_answer(user_answer)
+  user_answer = user_answer.downcase
   if user_answer == 'y'
     puts 'What are you searching for?'
     runscript
@@ -30,23 +31,46 @@ def html_builder(list)
   file = Nokogiri::HTML::Builder.new do |html|
     position = 1
     html.body do
-      html.node1 'POSITION  DESCRIPTION  LINK  SIZE  SEEDS' do
-        html.br
+      html.node1 do
         while position < list[0].length
-          html.div = position
-          html.div = list[0][position].text
-          link = list[1][position - 1].to_s
-          limit = link.index('>') - 2
-          html.div = 'https://thepiratebay.org' + link[9..limit]
-          html.div = list[2][position].text
-          html.div = list[3][position].text
-          html.br
-          position += 1
+          html.article do
+            html.span = position
+            html.a = list[0][position].text
+            link = list[1][position - 1].to_s
+            html.a = 'https://thepiratebay.org' + link[9..link.index('>') - 2]
+            html.div = 'SIZE  ' + list[2][position].text
+            html.div = list[3][position].text + '  SEEDS'
+            html.br
+            position += 1
+          end
         end
       end
     end
   end
   file.to_html
+end
+
+def html_format(file)
+  doc = Nokogiri::HTML.parse(file)
+  doc.xpath('//span').each do |tag|
+    tag['style'] = 'align-items: center; color: black; margin: 5px 20px;'
+  end
+
+  doc.xpath('//a').each do |tag|
+    tag['style'] = 'align-items: center; color: black; margin: 5px 50px; width: 20%; text-decoration: none;'
+  end
+
+  doc.xpath('//div').each do |tag|
+    tag['style'] = 'align-items: center; color: black; margin: 5px 30px;'
+  end
+
+  doc.xpath('//node1').each do |tag|
+    tag['style'] = 'align-items: center;'
+  end
+
+  doc.xpath('//article').each do |tag|
+    tag['style'] = 'display: flex; jutify-content: center; align-items: center; width: 100%; border: 1px black solid;'
+  end
 end
 
 def runscript
@@ -55,6 +79,7 @@ def runscript
   list = search.result
   show_results(list)
   file = html_builder(list)
+  file = html_format(file)
   File.open(user_input + '.html', 'w') { |f| f.write(file) }
   print('Do you want to make a new search? [Y]es or [N]o? ')
   check_answer(gets.chomp)
