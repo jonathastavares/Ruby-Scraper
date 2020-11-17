b376e9212b0760401c8035280520e73de94c6db3
Jonathas Tavares <jonathastavares@id.uff.br>
2020-11-17 20:29:30 -0300

    FIX linter errors

--- spec/display_spec.rb
+++ spec/display_spec.rb
@@ -15,15 +15,18 @@ describe Table do
   describe 'build_table' do
     it 'Checks if the table has been built correctly' do
       results = @table.build_table
-      expect(results[0]).to eq('    1         ONE PIECE World Seeker - PLAZA                                         https://thepiratebay.org/description.php?id=37075887   7.61 MiB       87')
-      expect(results[1]).to eq('    2         One Piece Season 1,2,3 {EnG SubbeD} [Episode 1-92] L@mBerT             https://thepiratebay.org/description.php?id=8764563    3.6 GiB        84')
+      expect(results[0]).to eq('    1         ONE PIECE World Seeker - PLAZA' \
+'                                         https://thepiratebay.org/description.php?id=37075887   7.61 MiB       87')
+      expect(results[1]).to eq('    2         One Piece Season 1,2,3 {EnG SubbeD} [Episode 1-92] L@mBerT' \
+'             https://thepiratebay.org/description.php?id=8764563    3.6 GiB        84')
     end
   end
 
   describe 'build_row' do
     it 'Checks if the row has been built correctly' do
       row = @table.build_row('1', @list[1], @links[0], @sizes[1], @seeds[1])
-      expect(row).to eq('    1         ONE PIECE World Seeker - PLAZA                                         https://thepiratebay.org/description.php?id=37075887   7.61 MiB       87')
+      expect(row).to eq('    1         ONE PIECE World Seeker - PLAZA' \
+'                                         https://thepiratebay.org/description.php?id=37075887   7.61 MiB       87')
     end
   end
 end
